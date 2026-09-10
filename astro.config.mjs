09d92be7531835a5fcdd0bfbcc4604448551d06d
respectedgenre1 <154222051+respectedgenre1@users.noreply.github.com>
2026-09-10 12:14:30 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap'; // <-- Add this import
 
 export default defineConfig({
-  site: 'https://247avlplug.com', // <-- Make sure your domain is here
+  site: '', // <-- Make sure your domain is here
   integrations: [sitemap()],      // <-- Add this line
 });
